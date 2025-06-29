3d900df67f689c4bf0edaa6d003d002d5d9f530e
Habeeb Adenle <haa009@latech.edu>
2025-06-29 08:46:45 -0500

    Adding constants for design

---
+++ config.py
@@ -0,0 +1,8 @@
+#####################################
+## config.py (to define constants) ##
+#####################################
+n = 4            # Total nodes
+t = 1            # Maximum Byzantine nodes
+IDs = [0, 1, 2, 3]
+KAPPA = 256      # Merkle tree hash size, if applicable
+

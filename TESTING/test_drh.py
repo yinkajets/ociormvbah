70e409c22d2361e193aa88f2a9a80e7ad4189658
Habeeb Adenle <haa009@latech.edu>
2025-07-06 16:49:44 -0500

    updating

--- TESTING/test_drh.py
+++ TESTING/test_drh.py
@@ -4,6 +4,10 @@
 ### test drh.py ###
 ###################
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from drh import DRh
 
 def test_drh():
